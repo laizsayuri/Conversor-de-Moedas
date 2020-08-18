73a832ec249f0a13d4c58e5ccb4d0eac74ac24a1
laizsayuri <69865590+laizsayuri@users.noreply.github.com>
2020-08-18 14:32:26 -0300

    Commit inicial e único do conversor.

---
+++ ConversorDeMoedas/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace aula01EAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIene = new System.Windows.Forms.Button();
+            this.tbTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDolar = new System.Windows.Forms.Button();
+            this.btnEuro = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIene
+            // 
+            this.btnIene.Location = new System.Drawing.Point(22, 65);
+            this.btnIene.Name = "btnIene";
+            this.btnIene.Size = new System.Drawing.Size(57, 35);
+            this.btnIene.TabIndex = 0;
+            this.btnIene.Text = "Iene ¥";
+            this.btnIene.UseVisualStyleBackColor = true;
+            this.btnIene.Click += new System.EventHandler(this.btnIene_Click);
+            // 
+            // tbTexto
+            // 
+            this.tbTexto.Location = new System.Drawing.Point(79, 21);
+            this.tbTexto.Name = "tbTexto";
+            this.tbTexto.Size = new System.Drawing.Size(180, 20);
+            this.tbTexto.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor R$:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado:";
+            // 
+            // btnDolar
+            // 
+            this.btnDolar.Location = new System.Drawing.Point(85, 65);
+            this.btnDolar.Name = "btnDolar";
+            this.btnDolar.Size = new System.Drawing.Size(60, 35);
+            this.btnDolar.TabIndex = 5;
+            this.btnDolar.Text = "Dólar $";
+            this.btnDolar.UseVisualStyleBackColor = true;
+            this.btnDolar.Click += new System.EventHandler(this.btnDolar_Click);
+            // 
+            // btnEuro
+            // 
+            this.btnEuro.Location = new System.Drawing.Point(151, 65);
+            this.btnEuro.Name = "btnEuro";
+            this.btnEuro.Size = new System.Drawing.Size(63, 35);
+            this.btnEuro.TabIndex = 6;
+            this.btnEuro.Text = " Euro €";
+            this.btnEuro.UseVisualStyleBackColor = true;
+            this.btnEuro.Click += new System.EventHandler(this.btnEuro_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(110, 115);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(0, 16);
+            this.lbResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 167);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnEuro);
+            this.Controls.Add(this.btnDolar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTexto);
+            this.Controls.Add(this.btnIene);
+            this.Name = "Form1";
+            this.Text = "Conversor de Moedas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIene;
+        private System.Windows.Forms.TextBox tbTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDolar;
+        private System.Windows.Forms.Button btnEuro;
+        private System.Windows.Forms.Label lbResultado;
+    }
+}
+
---
+++ ConversorDeMoedas/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace aula01EAD
+{
+    public partial class Form1 : Form
+    {
+        double valor = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIene_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(tbTexto.Text) * 0.052;
+            lbResultado.Text = valor.ToString() + " ¥";
+        }
+
+        private void btnDolar_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(tbTexto.Text) * 5.5 ;
+            lbResultado.Text = valor.ToString() + " $";
+        }
+
+        private void btnEuro_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(tbTexto.Text) * 6.57;
+            lbResultado.Text = valor.ToString() + " €";
+        }
+    }
+}
